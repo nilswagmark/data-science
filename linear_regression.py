085af40fe58bf6c1186ff19c3fd572ad84d8f36a
nwagmark <nwagmark@mavenoid.com>
2024-07-12 18:06:50 +0200

    added some new regression files

---
+++ linear_regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+import sklearn as sklearn
+from sklearn.linear_model import LinearRegression
+
+x = np.array([5, 15, 25, 35, 45, 55]).reshape((-1,1))
+y = np.array([5, 20, 14, 32, 22, 38])
+
+model = LinearRegression().fit(x,y)
+
+r_sq = model.score(x, y)
+intercept = model.intercept_
+slope = model.coef_
+print(r_sq, intercept, slope)
+
+y_pred = model.predict(x)
+
+
+
+
+
+
+
